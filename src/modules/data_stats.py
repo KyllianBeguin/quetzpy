f87713f726776f7b475931f3ac6e4187f69db6f8
Kyllian Beguin <50613619+KyllianBeguin@users.noreply.github.com>
2023-01-10 21:31:10 +0100

    [kb] Updated for consistency with OOP

--- src/modules/data_stats.py
+++ src/modules/data_stats.py
@@ -1,4 +1,4 @@
-def track_words(self, param = []) -> dict:
+def track_words(self, Params_manager, topic : str) -> dict:
     """
     View how many times a word is detected in the tweets\n
     Take a list of words to track\n
@@ -7,9 +7,8 @@ def track_words(self, param = []) -> dict:
 
     # Initialize the dict of {words: occurrence}
     dict_tracker = dict()
-
-    for to_track_word in param:
-        result = sum([1 for key, item in self._data.items() if to_track_word in item['text']])
+    for to_track_word in Params_manager.Extrac_track_words:
+        result = sum([1 for key, val in self.data_topics[topic]["tweets"].items() if to_track_word.lower() in val["text_cleaned"]])
         dict_tracker[to_track_word] = result
 
     return dict_tracker
@@ -18,22 +17,24 @@ def track_all():
     print("foo")
     return "foo"
 
-def show_occurrence(self, param = ["hashtags", 0]) -> dict:
+def show_occurrence(self, Params_manager, topic) -> dict:
     """
     View the occurrence of either linked hashtags or mentions\n
     Take a parameter to know if it shall analyze hashtags or mentions\n
     Returns a dict of words and the associated occurrence
     """
+
+    results = dict()
     
-    all_words = []
+    for show_this_occurence in Params_manager.Extrac_show_occurrence:
+        all_words = []
+        for key, val in self.data_topics[topic]["tweets"].items():
+            all_words.extend(val[show_this_occurence])
+            
+        all_words_uniques = set(all_words)
 
-    for key, item in self._data.items():
-        all_words.extend(item[param[0]])
+        result_for_this = {item:all_words.count(item) for item in all_words_uniques}
+        result_for_this_sorted = dict(filter(lambda item_sort : item_sort[1] >= Params_manager.Extrac_show_occurrence_threshold, sorted(result_for_this.items(), key = lambda item : item[1], reverse = True)))
+        results[show_this_occurence] = result_for_this_sorted
 
-    all_words_uniques = set(all_words)
-
-    result = {item:all_words.count(item) for item in all_words_uniques}
-    result_sorted = dict(filter(lambda item_sort : item_sort[1] > param[1], sorted(result.items(), key = lambda item : item[1], reverse = True)))
-
-
-    return result_sorted
+    return results
